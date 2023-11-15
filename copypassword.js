1cbdd62208157548035980c32e8b58edbb8e7ccd
Mel Scott <114887855+mirai64@users.noreply.github.com>
2023-11-15 10:24:53 -0800

    Added copy password option; renamed index.js to copypassword.js

---
+++ copypassword.js
@@ -0,0 +1,17 @@
+function copyPassword() {
+
+    var passInput = document.getElementById("passInput");
+
+    if (passInput.value.trim() === "") {
+        alert('Please generate a password.');
+        return;
+    }
+
+    navigator.clipboard.writeText(passInput.value)
+        .then(function() {
+            alert("Password copied!");
+        })
+        .catch(function(err) {
+            alert("Unable to copy password. Please try again.");
+        });
+}
