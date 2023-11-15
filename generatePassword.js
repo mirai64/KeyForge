f4145720e8568d608df1e7340d091a1d2b542c10
mirai64 <114887855+mirai64@users.noreply.github.com>
2023-11-15 13:56:35 -0800

    Created generatePassword.js file; added charset for password generation

---
+++ generatePassword.js
@@ -0,0 +1,11 @@
+function generatePassword() {
+    
+}
+
+
+let charset = "";
+
+if (uppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+if (lowercase) charset += "abcdefghijklmnopqrstuvwxyz"
+if (numbers) charset += "0123456789"
+if (symbols) charset += "!@#$%^&*()_+[]{}|;:,.<>?"
