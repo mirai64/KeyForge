5b0598d8d5e7001e21f4e84c6bfd569196f231cf
mirai64 <114887855+mirai64@users.noreply.github.com>
2023-11-15 15:04:19 -0800

    Added working password generation

--- index.js
+++ index.js
@@ -17,12 +17,41 @@ function copyPassword() {
 }
 
 let slider = document.getElementById("length")
-let output = document.getElementById("charRange")
+let output = document.getElementById("lengthValue")
 
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
     output.innerHTML = this.value;
+}
+
+function generatePassword() {
+    let length = document.getElementById("length").value;
+    let uppercase = document.getElementById("uppercase").checked;
+    let lowercase = document.getElementById("lowercase").checked;
+    let numbers = document.getElementById("numbers").checked;
+    let symbols = document.getElementById("symbols").checked;
+
+    let charset = "";
+
+    if (uppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if (lowercase) charset += "abcdefghijklmnopqrstuvwxyz"
+    if (numbers) charset += "0123456789"
+    if (symbols) charset += "!@#$%^&*()_+[]{}|;:,.<>?"
+
+    let password = "";
+        for (let i = 0; i < length; i++) {
+            let randomIndex = Math.floor(Math.random() * charset.length);
+            password += charset[randomIndex]
+        }
+
+        document.getElementById("password").value = password;
+
+}
+
+function updateLengthValue() {
+    let length = document.getElementById("length").value;
+    document.getElementById("lengthValue").innerText = length;
 }
 
 // link this to the Password Strength: portion
