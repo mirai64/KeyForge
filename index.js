de118559fe6d419ae87abbd6a3a17022ee9f91da
Mel Scott <114887855+mirai64@users.noreply.github.com>
2023-11-15 10:39:04 -0800

    Updated index.html; users can no longer type into password input form

--- index.js
+++
@@ -1,7 +0,0 @@
-function copyPassword() {
-    let copyText = document.getElementById("passInput");
-}
-
-copyText.select();
-copyText.setSelectionRange(0, 65);
-navigator.clipboard.writeText(copyText.value);
