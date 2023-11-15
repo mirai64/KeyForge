5272bec0551d2193b8644214ea8b54f47d94bed0
mirai64 <114887855+mirai64@users.noreply.github.com>
2023-11-15 13:41:21 -0800

    Moved all js files into one index.js file

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ function copyPassword() {
             alert("Unable to copy password. Please try again.");
         });
 }
+
+let slider = document.getElementById("passRange")
+let output = document.getElementById("charRange")
+
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+// link this to the Password Strength: portion
+// options are: Bad, Weak, Strong, Very Strong
+// Bad password: 5-7
+// Weak: 8-11
+// Strong: 12-24
+// Very Strong: 25-64
